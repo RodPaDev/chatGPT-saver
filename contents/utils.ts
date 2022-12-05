6664e767d254bd907e6efef4634125a05fced6de
Patrick <47316946+RodPaDev@users.noreply.github.com>
2022-12-05 16:46:35 +0000

    chatGPT logic

---
+++ contents/utils.ts
@@ -0,0 +1,99 @@
+import { DateTime } from 'luxon'
+import TurndownService from 'turndown'
+
+const turndownService = new TurndownService({ bulletListMarker: '-' })
+
+function getContentNodes(avatarDiv: Element) {
+  return Array.from(avatarDiv.parentElement.parentElement.children).find(
+    (item: Element) => !item.className.includes('ConversationItem__Role')
+  )
+}
+
+function createCodeBlock(code: Element) {
+  const isInlineCode = getComputedStyle(code, '::before').getPropertyValue(
+    'content'
+  )
+
+  const hljsLang = Array.from(code.classList).find((className) =>
+    className.includes('language')
+  )
+  const language = hljsLang?.split('-')?.at(-1) || ''
+
+  if (
+    isInlineCode &&
+    !language &&
+    code?.parentElement?.tagName?.toLowerCase() === 'p'
+  ) {
+    return turndownService.turndown(code.parentElement.innerHTML)
+  }
+
+  const block = `\`\`\``
+  // this weird formatting is because I'm too lazy to sanitize it properly
+  // prettier-ignore
+  return ` 
+\n${block}${language}
+${code.textContent.trim()}
+${block}\n`
+}
+
+export function getConversationThread(conversationItems: Element[]) {
+  let thread = []
+  for (let item of conversationItems) {
+    const avatar = item.querySelector('[class*="Avatar__Wrapper"]')
+    if (avatar) {
+      const contentNodes = getContentNodes(avatar)
+
+      const content = Array.from(contentNodes.children).find(
+        ({ className }) =>
+          !className.includes('ConversationItem__ActionButtons')
+      )
+
+      thread.push({
+        sender: 'User',
+        content: content.textContent
+      })
+    } else {
+      const messageItem = Array.from(item.children).find(({ className }) =>
+        className.includes('ConversationItem__Message')
+      )
+
+      const contentNodes = Array.from(messageItem.children).find(
+        ({ className }) => !className.includes('ConversationItem__Role')
+      )
+
+      const content = Array.from(contentNodes.children).find(
+        ({ className }) =>
+          !className.includes('ConversationItem__ActionButtons')
+      )
+
+      const textContentNodes = Array.from(content.children[0].children)
+
+      let textContent = ``
+
+      for (let textNode of textContentNodes) {
+        const code = textNode.querySelector('code')
+
+        textContent += code
+          ? createCodeBlock(code)
+          : turndownService.turndown(textNode.outerHTML) + '\n'
+      }
+      thread.push({
+        sender: 'ChatGPT',
+        content: textContent
+      })
+    }
+  }
+  return thread
+}
+
+export function downloadFile(markdownString: string) {
+  const blob = new Blob([markdownString], { type: 'text/plain' })
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement('a')
+
+  const date = DateTime.now().toFormat('yyyy-LL-dd_HH-mm')
+
+  link.download = `ChatGPT-${date}.txt`
+  link.href = url
+  link.click()
+}
